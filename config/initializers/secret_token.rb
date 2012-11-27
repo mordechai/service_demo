5a05931e6b22f56490166940db78c0421baa3415
root <root@server-1350221565-az-3-region-a-geo-1.novalocal>
2012-11-27 11:21:47 +0000

    rails_apps_composer: initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Servicedemo::Application.config.secret_token = 'fcde4b44977945a8887eba275a8bbb721d9c9c177b42d3ce1438c955fdd8fc8e2598d7158abaefa9398ed2c3c67bfedef7e0f1599fd6de9cda1560bc3fe35a93'
+Servicedemo::Application.config.secret_token = 'beef4791655061737abff68ccc49e8392c55fd9eebfea205d0b98064ca69b5b26417af9923bb99b1a872aed5b21b666f0a19bba99eaae4d7174914ac0101e88f'
